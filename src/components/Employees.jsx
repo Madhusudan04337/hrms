4c262a652ee3c13b3a35301fb4dbdcd0689df924
Madhusudan <madhusudan04337@gmail.com>
2025-07-12 21:09:48 +0530

    create page content for dashboard, chat, and employees

---
+++ src/components/Employees.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Employees = () => { 
+    return (
+        <div>
+            <h2>Employee</h2>
+            <p>Welcome to the employee management section!</p>
+        </div>
+    );
+}
+
+export default Employees;
