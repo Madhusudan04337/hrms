f89b57d38442efe7d5d2e08e94290c22050e4c79
Naresh-V2005 <vnaresh102005@gmail.com>
2025-07-14 17:03:43 +0530

    react icons updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {FaTachometerAlt, FaComments, FaUsers,FaRegThumbsUp, FaCalendarAlt, FaUserCircle,FaCog} from 'react-icons/fa';
+import {FaTachometerAlt,FaChartBar,FaBookOpen, FaComments, FaUsers, FaCalendarAlt, FaUserCircle,FaCog} from 'react-icons/fa';
 import '../styles/Navbar.css';
 
 console.log("navbar loaded");
@@ -39,8 +39,8 @@ const Sidebar = ( {setActivePage,activePage}) => {
                 </li>
                 <li className='w-100 mb-1'><button onClick={() => setActivePage('chat')} className={`nav-link text-white w-100 text-start ${activePage === 'chat'?'active':''}`}><FaComments className='me-2' />Chat</button></li>
                 <li className='w-100 mb-1'><button onClick={() => setActivePage('employees')} className={`nav-link text-white w-100 text-start ${activePage === 'employees'?'active':''}`}><FaUsers className='me-2' />Employees</button></li>
-                <li className='w-100 mb-1'><button onClick={() => setActivePage('feed')} className={`nav-link text-white w-100 text-start ${activePage === 'feed'?'active':''}`}><FaRegThumbsUp className='me-2' />Feed</button></li>
-                <li className='w-100 mb-1'><button onClick={() => setActivePage('recogination')} className={`nav-link text-white w-100 text-start ${activePage === 'recogination'?'active':''}`}><FaRegThumbsUp className='me-2' />Recogination</button></li>
+                <li className='w-100 mb-1'><button onClick={() => setActivePage('feed')} className={`nav-link text-white w-100 text-start ${activePage === 'feed'?'active':''}`}><FaBookOpen className='me-2' />Feed</button></li>
+                <li className='w-100 mb-1'><button onClick={() => setActivePage('recogination')} className={`nav-link text-white w-100 text-start ${activePage === 'recogination'?'active':''}`}><FaChartBar className='me-2' />Recogination</button></li>
                 <li className='w-100 mb-1'><button onClick={() => setActivePage('event')} className={`nav-link text-white w-100 text-start ${activePage === 'event'?'active':''}`}><FaCalendarAlt className='me-2' />Event</button></li>
                 <li className='w-100 mb-1'><button onClick={() => setActivePage('profile')} className={`nav-link text-white w-100 text-start ${activePage === 'profile'?'active':''}`}><FaUserCircle className='me-2' />Profile</button></li>
                 <li className='w-100 mb-1'><button onClick={() => setActivePage('settings')} className={`nav-link text-white w-100 text-start ${activePage === 'settings'?'active':''}`}><FaCog className='me-2' />Settings</button></li>
